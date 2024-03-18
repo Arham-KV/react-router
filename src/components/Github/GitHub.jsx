50e598ca85eefeb0407055e241761131596204e6
Arham <syedarham032@gmai.com>
2024-03-18 12:30:28 +0500

    first-route

---
+++ src/components/Github/GitHub.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+function GitHub() {
+    useEffect(() => {
+
+    })
+    return (
+        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>GitHub Followers:3000</div>
+    )
+}
+
+export default GitHub
